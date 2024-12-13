149919bc0b8efe5e32eaa8c6bd088bc147260ad7
Kyley12 <junseoyang760@gmail.com>
2024-12-13 17:29:33 +0900

    camera

--- BossRush/Assets/Runtime/_Scripts/Camera/Camera.cs
+++ BossRush/Assets/Runtime/_Scripts/Camera/Camera.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using Codice.Client.Common.GameUI;
 using UnityEngine;
 
 public class Camera : MonoBehaviour
 {
-
+    public Transform playerTransform;
     private void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        transform.position = new Vector3(playerTransform.position.x, transform.position.y, playerTransform.position.z);
     }
 }
